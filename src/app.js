4b5fe12042fb3e42487083410a60460dbafc11d1
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-01-01 01:14:43 +0200

    chore: setup sequelize

--- src/app.js
+++ src/app.js
@@ -2,8 +2,30 @@ import express from "express";
 import exphbs from "express-handlebars";
 import bodyParser from "body-parser";
 import path from "path";
+import config from "./config/config";
 
 const app = express();
+
+const { DB_NAME, DB_USERNAME, DB_PASSWORD } = config;
+
+import Sequelize from "sequelize";
+const db = new Sequelize(`${DB_NAME}`, `${DB_USERNAME}`, `${DB_PASSWORD}`, {
+    host: 'localhost',
+    dialect: 'postgres',
+    operatorsAliases: false,
+    
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+// Test DB
+db.authenticate()
+    .then(() => console.log(`Database connected...`))
+    .catch(err => console.log(`Error: ${err}`));
 
 app.get('/', (req, res) => res.send('INDEX'));
 
---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export default {
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD
+};
