9bda476d1f55e0015e8a76f2a9da56a12f6d94b8
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-01-03 20:42:11 +0200

    Connecting to test database

--- src/app.js
+++ src/app.js
@@ -1,28 +1,46 @@
-import express from "express";
-import exphbs from "express-handlebars";
-import bodyParser from "body-parser";
-import path from "path";
-import config from "./config/config";
+import { Client } from 'pg';
 
-// Database
-import { db } from "./config/database";
+const client = new Client({
+    user: 'postgres',
+    password: 'samuel123',
+    host: 'localhost',
+    port : 5432,
+    database: 'test'
+}); 
 
-// Gigs routes
-import gigsRoutes from "./routes/gigs";
+client.connect()
+    .then(() => console.log('Connected successfully...'))
+    .then(() => client.query("select * from person where country_of_birth = $1", ["United States"]))
+    .then(results => console.table(results.rows))
+    .catch(e => console.log('Error' + e))
+    .finally(() => client.end())
 
-const app = express();
 
-// Test DB
-db.authenticate()
-    .then(() => console.log(`Database connected...`))
-    .catch(err => console.log(`Error: ${err}`));
+// import express from "express";
+// import exphbs from "express-handlebars";
+// import bodyParser from "body-parser";
+// import path from "path";
+// import config from "./config/config";
+
+// // Database
+// import { db } from "./config/database";
+
+// // Gigs routes
+// import gigsRoutes from "./routes/gigs";
+
+// const app = express();
+
+// // Test DB
+// db.authenticate()
+//     .then(() => console.log(`Database connected...`))
+//     .catch(err => console.log(`Error: ${err}`));
     
-// db.close();
+// // db.close();
 
-// routes
-app.get('/', (req, res) => res.status(200).send('INDEX'));
-app.use('/gigs', gigsRoutes);
+// // routes
+// app.get('/', (req, res) => res.status(200).send('INDEX'));
+// app.use('/gigs', gigsRoutes);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
+// app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
