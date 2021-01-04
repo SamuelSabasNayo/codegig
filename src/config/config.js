20ff7ba66295d08f253709f3889f7837d0a4ec36
SamuelSabasNayo <nshusamuel1@gmail.com>
2021-01-04 10:00:11 +0200

    Postgres & Sequelize connections

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export default {
-    DB_NAME: process.env.DB_NAME,
-    DB_USERNAME: process.env.DB_USERNAME,
-    DB_PASSWORD: process.env.DB_PASSWORD
-};
